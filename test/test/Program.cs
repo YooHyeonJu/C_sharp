90490532ebce0d7ba5f118ce3e1c0b2f7949903d
yhj8056@naver.com <guswn0503!>
2018-07-06 15:29:02 +0900

    add test

---
+++ test/test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate void Delegate();
+
+namespace test
+{    
+
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            foreach (string s in args)
+                Console.WriteLine(s);
+        }
+    }
+}
